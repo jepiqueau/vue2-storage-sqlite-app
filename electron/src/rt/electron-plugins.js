a026dfa3b5cb58085f80b8c2b38ed9f4ce5f026b
jepiqueau <jepi.queau@free.fr>
2021-09-29 17:02:47 +0200

    add electron platform

---
+++ electron/src/rt/electron-plugins.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const CapacitorDataStorageSqlite = require('/Users/queaujeanpierre/Development/vue2/vue2-storage-sqlite-app/electron/node_modules/capacitor-data-storage-sqlite/electron/dist/plugin.js')
+
+module.exports = {
+  CapacitorDataStorageSqlite,
+}
